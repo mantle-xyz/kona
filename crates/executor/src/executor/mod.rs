9404ae24aa8a335f41035bd878dc46302bd93406
PinelliaC <wylbzc4928@gmail.com>
2025-02-27 15:15:26 +0800

    add log

--- crates/executor/src/executor/mod.rs
+++ crates/executor/src/executor/mod.rs
@@ -198,7 +198,7 @@ where
 
             // Accumulate the gas used by the transaction.
             cumulative_gas_used += result.gas_used();
-
+            info!("tx type: {:?}", transaction.tx_type());
             // Create receipt envelope.
             let receipt = OpReceiptEnvelope::<Log>::from_parts(
                 result.is_success(),
